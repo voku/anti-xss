3003e1472cc048b3c3c7c82a17b04d14e1328800
Lars Moelleken <lmoelleken@menadwork.com>
2015-03-16 16:55:26 +0100

    [+]: fixed test

--- tests/XssTest.php
+++ tests/XssTest.php
@@ -68,7 +68,7 @@ class XssTest extends PHPUnit_Framework_TestCase {
       "<BR SIZE=\"&{alert('XSS')}\">" => "<BR SIZE=\"&{alert&#40;'XSS'&#41;}\">", // & JavaScript includes
       "<LINK REL=\"stylesheet\" HREF=\"javascript:alert('XSS');\">" => "&lt;LINK REL=\"stylesheet\" HREF=\"alert&#40;'XSS'&#41;;\"&gt;", // STYLE sheet
       "<STYLE>BODY{-moz-binding:url(\"http://ha.ckers.org/xssmoz.xml#xss\")}</STYLE>" => "&lt;STYLE&gt;BODY{:url(\"http://ha.ckers.org/xssmoz.xml#xss\")}&lt;/STYLE&gt;", // Remote style sheet
-      "<STYLE>@im\\port'\\ja\vasc\ript:alert(\"XSS\")';</STYLE>" => "&lt;STYLE&gt;@im\port'\jaasc\ript:alert&#40;\"XSS\"&#41;';&lt;/STYLE&gt;", // STYLE tags with broken up JavaScript for XSS
+      "<STYLE>@im\\port'\\ja\vasc\ript:alert(\"XSS\")';</STYLE>" => "&lt;STYLE&gt;@im\port'\ja\vasc\ript:alert&#40;\"XSS\"&#41;';&lt;/STYLE&gt;", // STYLE tags with broken up JavaScript for XSS
       "<XSS STYLE=\"xss:expression_r(alert('XSS'))\">" => "", // Anonymous HTML with STYLE attribute
       "<XSS STYLE=\"behavior: url(xss.htc);\">" => "", // Local htc file
       "¼script¾alert(¢XSS¢)¼/script¾" => "¼script¾alert&#40;¢XSS¢&#41;¼/script¾", // US-ASCII encoding
