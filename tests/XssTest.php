6bdc6c38f0064dcb06db07e6f6d1e9c550fb441a
lmoelleken <lmoelleken@meerx-it.com>
2021-10-03 23:24:44 +0200

    [+]: add test for issue #78

--- tests/XssTest.php
+++ tests/XssTest.php
@@ -125,6 +125,7 @@ final class XssTest extends \PHPUnit\Framework\TestCase
             '<3'                                                                                                                                                                                                                                      => '<3',
             '≪2 €'                                                                                                                                                                                                                                    => '≪2 €',
             '<35%'                                                                                                                                                                                                                                    => '<35%',
+            '< 35%' => '< 35%',
         ];
 
         $antiXss->removeEvilAttributes(['style']); // allow style-attributes
