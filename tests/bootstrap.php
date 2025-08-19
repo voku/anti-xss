a4f64d70cd2de73de90c0c48dfe40428a935b950
vcozmulici <vcozmulici@seedbox.com>
2025-08-19 16:15:47 -0400

    TC-29805-Migrate-projects

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,16 @@
 
 require_once \dirname(__DIR__) . '/vendor/autoload.php';
 
-// Include the UTF8 class fix
-require_once __DIR__ . '/bootstrap_fix.php';
+class UTF8Initializer {
+    public static function init(): void {
+        // Call a method that will initialize the support arrays
+        // This will trigger the static initialization in the UTF8 class
+        \voku\helper\UTF8::checkForSupport();
+        
+        // Suppress warnings for the test run
+        \error_reporting(\E_ALL & ~\E_WARNING & ~\E_DEPRECATED & ~\E_USER_DEPRECATED);
+    }
+}
+
+// Initialize the UTF8 class
+UTF8Initializer::init();
